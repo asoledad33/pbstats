5d56c7170554d9ea60ca9ed3a79af064c3a5109c
Alejandro <alexsol470@gmail.com>
2022-03-11 08:25:26 -0400

    adding package files along with setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+DESCRIPTION = 'A package that extracts data from a json file and displays statistics'
+
+setuptools.setup(
+    name="plotbeeStatistics",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    python_requires='>=3.6',
+)
