2f9032b5287036c93cc5b01174cfa0cacf1358f8
Alejandro <alexsol470@gmail.com>
2022-03-11 14:31:18 -0400

    adding return value to stats.py

--- pbstats/stats.py
+++ pbstats/stats.py
@@ -4,4 +4,4 @@ import pandas as pd
 def display_statistics_table(augmented_visit_df):
     flower_statistics = augmented_visit_df.groupby("Flower").agg({'Visit Duration (s)':['mean','count']})
 
-    flower_statistics
+    return flower_statistics
