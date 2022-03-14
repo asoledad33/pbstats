be4b4afc3ba5bdf0db8c2f1d47391b01b9a9b9d2
Alejandro <alexsol470@gmail.com>
2022-03-14 01:53:11 -0400

    Adding return value to show_statistics_table function

--- pbstats/stats.py
+++ pbstats/stats.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def display_statistics_table(augmented_visit_df):
+def show_statistics_table(augmented_visit_df):
     flower_statistics = augmented_visit_df.groupby("Flower").agg({'Visit Duration (s)':['mean','count']})
 
     return flower_statistics
